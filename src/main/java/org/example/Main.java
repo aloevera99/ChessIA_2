2a489a6576f9d058814f60881b68845b981a6dec
aloevera99 <94184153+aloevera99@users.noreply.github.com>
2022-11-02 14:27:23 -0400

    Prise en main chesslib et restructuration organisation

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,8 +2,11 @@ package org.example;
 
 
 import com.github.bhlangonijr.chesslib.Board;
+import com.github.bhlangonijr.chesslib.Piece;
 import com.github.bhlangonijr.chesslib.Square;
 import com.github.bhlangonijr.chesslib.move.Move;
+
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
@@ -16,6 +19,24 @@ public class Main {
         //print the chessboard in a human-readable form
         System.out.println(board.toString());
 
-        System.out.println("Hello world!");
+        //Find the square locations of black bishops
+        List<Square> blackBishopSquares = board.getPieceLocation(Piece.BLACK_BISHOP);
+
+        //Get the piece at A1 square...
+        Piece piece = board.getPiece(Square.A1);
+
+        System.out.println("la position des bishops noir est la suivante :\n");
+
+        for (int i = 0; i < blackBishopSquares.size(); i++){
+            System.out.println(blackBishopSquares.get(i));
+        }
+
+        System.out.println("la pièce en A1 est la suivante :\n");
+        System.out.println(piece);
+
+        // Generate legal chess moves for the current position
+        List<Move> moves = board.legalMoves();
+        System.out.println("Legal moves: " + moves);
+
     }
 }
