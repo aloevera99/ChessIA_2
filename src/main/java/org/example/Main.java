fb385c24ba9141d55c03966102e0dda5726444ec
aloevera99 <94184153+aloevera99@users.noreply.github.com>
2022-11-02 13:57:08 -0400

    Add and set up library chesslib

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,21 @@
 package org.example;
 
+
+import com.github.bhlangonijr.chesslib.Board;
+import com.github.bhlangonijr.chesslib.Square;
+import com.github.bhlangonijr.chesslib.move.Move;
+
 public class Main {
     public static void main(String[] args) {
+        // Creates a new chessboard in the standard initial position
+        Board board = new Board();
+
+        //Make a move from E2 to E4 squares
+        board.doMove(new Move(Square.E2, Square.E4));
+
+        //print the chessboard in a human-readable form
+        System.out.println(board.toString());
+
         System.out.println("Hello world!");
     }
 }
