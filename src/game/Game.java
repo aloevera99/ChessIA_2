829092698bf1caf5b3a70d504d3e91b8ae04bff9
aloevera99 <94184153+aloevera99@users.noreply.github.com>
2022-11-02 15:00:51 -0400

    create and start coding game class and minimax class

---
+++ src/game/Game.java
@@ -0,0 +1,18 @@
+import com.github.bhlangonijr.chesslib;
+import java.util.*;
+
+class Game {
+
+    private boolean turn; //false means white turn, true means black turn    private boolean victory; //false means game has not been won, true means game is over
+    private boolean is_white; //true means AI is white, false means AI is black
+
+    public Game(boolean is_white){
+        //sets who is what color
+        is_white = is_white;
+        if (is_white)
+            turn = false;
+        else
+            turn = true;
+    }
+
+}
---
+++ src/minimax/Minimax.java
@@ -0,0 +1,15 @@
+import com.github.bhlangonijr.chesslib.*;
+import java.util.*;
+
+class Minimax {
+
+    Move bestMove;
+    int bestMoveScore;
+
+    ArrayList<Board> possileBoards = new ArrayList<Board>();
+
+
+
+
+
+}
