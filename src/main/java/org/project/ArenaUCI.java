3ce99bcb1320629a8356caa34911ee7718ef60f2
Juliette Le Meudec <94185070+Bluje@users.noreply.github.com>
2022-11-22 16:02:28 -0500

    UpUpdate UCI

--- src/main/java/org/project/ArenaUCI.java
+++ src/main/java/org/project/ArenaUCI.java
@@ -1,6 +1,7 @@
 package org.project;
 
-import com.github.bhlangonijr.chesslib.Board;
+import com.github.bhlangonijr.chesslib.*;
+import com.github.bhlangonijr.chesslib.move.Move;
 import com.github.bhlangonijr.chesslib.move.MoveList;
 
 import java.util.*;
@@ -48,22 +49,18 @@ class ArenaUCI {
             //System.out.println("id author Jonathan");
             System.out.println("uciok");
         }
-
         public static void inputIsReady() {
             System.out.println("readyok");
         }
 
         public static void inputPosition(String input){
             input = input.substring(9).concat("");
-            Board board = new Board(); //generate initial board / pas la bonne instruction je pense
 
             if (input.contains("startpos")){
                 input= input.substring(9); //remove the word "position"
-
             }
             else if (input.contains("fen")){
-                // j'ai pqs trop compris ce qu'il se passe la
-            //générer le board actuel
+                // On a besoin de ca ?
             }
             else if (input.contains("moves")){
                 input = input.substring(input.indexOf("moves")+6); // remove the word "moves"
@@ -71,31 +68,19 @@ class ArenaUCI {
                 MoveList list = new MoveList();
                 list.loadFromSan(input);
                 board.loadFromFen(list.getFen());
-
-                while (input != ""){
-                //récupérer les deux premiers caracteres et les mettre dans dep
-                // récupérer les deux caracteres suivants et les mettre dans arr
-                // faire le move -> Move (dep,arr);
-
-                //checker la couleur
-                    //if IA, faire le move de l'IA
-                    // else if JOUEUR, faire le move du joueur
-                //faire le move en conséquence
-                }
             }
         }
         public static void inputNewGame(){
             Board board = new Board(); // generate new Board for a new game
-            // remettre l'engine a zéro -> normalement c'est ok
+            // remettre l'engine a zéro
         }
         public static void inputGo(){
-            //IA.minimax(board); //search for the best move
+            Move bestmove = IA.minimax(board); //search for the best move
+            System.out.println("bestmove" + bestmove);
         }
 
        /*
        //pas sûr que ce soit utile
        public static void inputPrint(){
-
         }*/
-
 }
